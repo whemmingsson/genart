6d4f5c7e8fb1237307c2262c0b83906c3fdb90d2
William Hemmingsson <william.hemmingsson@gmail.com>
2021-09-30 01:05:29 +0200

    Added code for 'palette from image'

--- color_palette_from_image/js/sketch.js
+++ color_palette_from_image/js/sketch.js
@@ -1,9 +1,73 @@
+let img;
+let colors = [];
+let cellSize;
+let sortMode = null;
+let sortModeMap = null;
 
+const CELL_COUNT = 50;
+
+function preload() {
+  img = loadImage("../img/monalisa.png")
+}
 
 function setup() {
-  createCanvas(1200, 850);  
+  createCanvas(700, 700);  
+  ellipseMode(CORNER);
+
+  initDefaultValues();
+  createColorArrayFromImage(); 
 }
 
 function draw() {
-  background(50);
+  background(0);
+  sortColors();
+  renderCells(cellSize);
+}
+
+function createColorArrayFromImage() {
+  img.loadPixels();
+
+  for (let y = 0; y < CELL_COUNT; y++) {
+    for (let x = 0; x < CELL_COUNT; x++) {
+      colors.push(createColor(x, y, cellSize));
+    }
+  }
+}
+
+function initDefaultValues() {
+  sortModeMap = { h: hue, s: saturation, b:brightness, 1: red, 2: green, 3: blue };
+  cellSize = width / CELL_COUNT;
+  sortMode = hue;
+}
+
+function keyTyped() {
+  if(sortModeMap[key])
+    sortMode = sortModeMap[key];
+  return false;
+}
+
+function renderCells(cellSize) {
+  noStroke();
+  let i = 0;
+  for (let y = 0; y < CELL_COUNT; y++) {
+    for (let x = 0; x < CELL_COUNT; x++) {
+      fill(colors[i]);
+      ellipse(x * cellSize, y * cellSize,cellSize, cellSize)
+      // rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      i++;
+    }
+  }
+}
+
+function createColor(x, y, cellSize){
+  const index = (int(y*cellSize) * img.width + int(x*cellSize)) * 4;
+  return color(img.pixels[index], img.pixels[index+1], img.pixels[index+2], 255);
+}
+
+function sortColors(){
+  colors.sort((a,b) => colorCompare(a,b, sortMode));
+}
+
+function colorCompare(a,b, method) {
+  return method(a) - method(b);
 }
